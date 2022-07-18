b13e23d010e616a463cfd4d29b76da3fc7543f5f
Ross Gordon <rossagordonstl@gmail.com>
2022-07-18 14:58:24 -0400

    Navbar complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu} >
+            Buddy's Coffee <i class="fa-solid fa-mug-hot"></i>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/order-online"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Order Online
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline ">ORDER ONLINE</Button>}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
